a94b655a91acd8e2a713d690788b465d7ffb83bf
gomi <gxy880520@qq.com>
2024-03-02 23:48:47 +0800

    perf: remove umd version

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             entry: resolve(__dirname, 'index.ts'),
             name: 'audiohacker',
             fileName: 'audiohacker',
-            formats: ['es', 'umd']
+            formats: ['es']
         },
         rollupOptions: {
             treeshake: false
